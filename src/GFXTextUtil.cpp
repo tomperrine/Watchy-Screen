3faded513e20f407f16de7f4043b97e872067d06
Charles Haynes <ceh+github@ceh.bz>
2021-08-23 21:24:39 +1000

    text wrap working. unoptimized

--- src/GFXTextUtil.cpp
+++ src/GFXTextUtil.cpp
@@ -61,6 +61,7 @@ void drawWordWrappedText(Adafruit_GFX &g, int16_t x, int16_t y, int16_t w,
   g.setFont(f);
   g.setTextWrap(false);  // clip (GFX used to have a bug that would auto-wrap on
                          // exact width)
+  const char *origT = t;
   const char *startLine = t;
   const char *wordBreak = t;
   int16_t wordBreakXPos = 0;
@@ -68,44 +69,75 @@ void drawWordWrappedText(Adafruit_GFX &g, int16_t x, int16_t y, int16_t w,
   int16_t yPos = f->yAdvance;  ///< y position relative to top of bounding box
   bool inWord = false;
   for (char c = *t;; c = *++t) {
-    LOGD("%3d %3d %3d %2d %1c %02x", w, wordBreakXPos, xPos, charWidth(c, f), c, c);
-    if (c != '\0' && c != '\n') {
-      if (inWord != !isspace(c)) {
-        wordBreak = t;
+    LOGD("%3d %3d %3d %3d %2d %02x %1c", w, t-origT, wordBreakXPos, xPos, charWidth(c, f), c, c);
+    if (c == '\0') {
+      // end of text
+      g.setCursor(x, y+yPos);
+      for (c=*startLine;*startLine;c=*++startLine) { g.print(c); }
+      return;
+    }
+    if (c == '\n') {
+      // newline character
+      g.setCursor(x, y+yPos);
+      for (c=*startLine; startLine<t; c=*++startLine) { g.print(c); }
+      yPos += f->yAdvance;
+      startLine = t+1;
+      inWord = false;
+      xPos = 0;
+      continue;
+    }
+    uint16_t cw = charWidth(c,f);
+    if (xPos+cw > w) {
+      // break this line
+      if (isSpace(c)) {
+        // at a space, break here
+        g.setCursor(x, y + yPos);
+        LOGD("\"%.*s\"", t-startLine, startLine);
+        for (c=*startLine; startLine<t; c=*++startLine) { g.print(c); }
+        // consume any remaining trailing whitespace
+        while (isspace(*t) && *t != '\n') {t++;}
+        if (*t == '\n') { t++; }
+        yPos += f->yAdvance;
+        startLine = t;
+        xPos = cw;
+        continue;
+      } else if (startLine == wordBreak) {
+        // word doesn't fit on a line. Hard break.
+        g.setCursor(x, y + yPos);
+        LOGD("\"%.*s\"", t-startLine, startLine);
+        for (c=*startLine; startLine<t; c=*++startLine) { g.print(c); }
+        yPos += f->yAdvance;
+        // startLine = t
+        wordBreak = startLine;
+        xPos = cw;
         wordBreakXPos = xPos;
-        inWord = !inWord;
-      }
-      xPos += charWidth(c, f);
-      if (xPos <= w) {
-        if (yPos + charDescent(c, f) > h) {
-          // text won't fit in bounding box. Done.
-          return;
-        }
+        continue;
+      } else {
+        // inside a word, break at start of word
+        g.setCursor(x, y + yPos);
+        LOGD("\"%.*s\"", wordBreak-startLine, startLine);
+        for (c=*startLine; startLine<wordBreak; c=*++startLine) { g.print(c); }
+        yPos += f->yAdvance;
+        // startLine = wordBreak;
+        xPos -= wordBreakXPos;
+        xPos += cw;
+        wordBreakXPos = 0;
         continue;
       }
-      // char won't fit on line, fall through
+    } else {
+      // this character fits within the width
+      if (yPos+charDescent(c,f) > h) { return; }
+      if (isSpace(c)) {
+        inWord = false;
+        wordBreak = t+1;
+        wordBreakXPos = xPos+cw;
+      } else if (!inWord) {
+        // start of a new word
+        inWord = true;
+        wordBreak = t;
+        wordBreakXPos = xPos;
+      }
+      xPos += cw;
     }
-    // wrap
-    if (startLine == wordBreak) {
-      // word doesn't fit on line. Hard wrap
-      wordBreak = t-1;
-      wordBreakXPos = xPos-charWidth(c, f);
-    }
-    g.setCursor(x, y + yPos);
-    for (; startLine < wordBreak; startLine++) {
-      g.print(*startLine);
-    }
-    if (!inWord) {
-      for (; *t && isSpace(*t) && *t != '\n'; t++) { }
-      // consume one leading '\n' since we just started a line
-      if (*t == '\n') { t++; }
-      if (*t == '\0') { return; }
-      startLine = t;
-      wordBreak = t;
-      inWord = !isspace(*t);
-    }
-    xPos -= wordBreakXPos;
-    yPos += f->yAdvance;
-    wordBreakXPos = 0;
   }
 }
